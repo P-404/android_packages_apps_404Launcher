b74ec018d00f7192e9092766af8adf3e92dd1f06
Amir Zaidi <azaidi@live.nl>
2020-01-11 13:55:58 +0100

    [SQUASHED] 404Launcher: Add Icon Pack Support from Shade Launcher
    @DennySPb: port to A11, A12-L
    
    Change-Id: Id81da397d5ef33577d6f53f349080c49a61591ab
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -18,6 +18,7 @@ package com.android.launcher3;
 
 import static com.android.launcher3.model.data.ItemInfoWithIcon.FLAG_ICON_BADGED;
 import static com.android.launcher3.util.Executors.MODEL_EXECUTOR;
+import com.android.launcher3.icons.IconProvider;
 
 import android.annotation.TargetApi;
 import android.app.ActivityManager;
@@ -129,6 +130,9 @@ public final class Utilities {
 
     public static final boolean ATLEAST_S = BuildCompat.isAtLeastS()
             || Build.VERSION.SDK_INT >= Build.VERSION_CODES.S;
+
+    public static final boolean ATLEAST_OREO =
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.O;
 
     private static final long WAIT_BEFORE_RESTART = 250;
 
@@ -699,8 +703,7 @@ public final class Utilities {
             LauncherActivityInfo activityInfo = context.getSystemService(LauncherApps.class)
                     .resolveActivity(info.getIntent(), info.user);
             outObj[0] = activityInfo;
-            return activityInfo == null ? null : LauncherAppState.getInstance(context)
-                    .getIconProvider().getIcon(
+            return activityInfo == null ? null : IconProvider.INSTANCE.get(context).getIcon(
                             activityInfo, activity.getDeviceProfile().inv.fillResIconDpi);
         } else if (info.itemType == LauncherSettings.Favorites.ITEM_TYPE_DEEP_SHORTCUT) {
             if (info instanceof PendingAddShortcutInfo) {
