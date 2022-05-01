3bb6d4583a4c78318e5189615992f67b94caf877
Pranav Vashi <neobuddy89@gmail.com>
2022-05-01 12:19:38 +0530

    404Launcher: Improve taskbar state controller for resume
    * Reduces weird animation replays after going homescreen from an app.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ica9c2e4d5b047ccd7e246489127d8defd530a475
    Signed-off-by: Akash Srivastava <akashniki@gmail.com>

--- quickstep/src/com/android/launcher3/taskbar/TaskbarLauncherStateController.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarLauncherStateController.java
@@ -211,22 +211,23 @@ import java.util.function.Supplier;
             boolean isResumed = isResumed();
             ObjectAnimator anim = mIconAlignmentForResumedState
                     .animateToValue(isResumed && goingToUnstashedLauncherState()
-                            ? 1 : 0)
-                    .setDuration(duration);
+                            ? 1 : 0);
+            if (isResumed) {
+                anim.setDuration(duration);
+            }
 
             anim.addListener(new AnimatorListenerAdapter() {
                 @Override
                 public void onAnimationEnd(Animator animation) {
                     mIsAnimatingToLauncherViaResume = false;
+                    TaskbarStashController stashController = mControllers.taskbarStashController;
+                    stashController.updateStateForFlag(FLAG_IN_APP, !isResumed);
+                    stashController.applyState(duration);
                 }
 
                 @Override
                 public void onAnimationStart(Animator animation) {
-                    mIsAnimatingToLauncherViaResume = isResumed;
-
-                    TaskbarStashController stashController = mControllers.taskbarStashController;
-                    stashController.updateStateForFlag(FLAG_IN_APP, !isResumed);
-                    stashController.applyState(duration);
+                    mIsAnimatingToLauncherViaResume = isResumed();
                 }
             });
             animatorSet.play(anim);
